ee19129f5eecea78de4a7bf19f4b06a102442201
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2025-05-01 18:56:54 +0300

    add socket creation

--- main.cpp
+++ main.cpp
@@ -1,6 +1,79 @@
+#include <winsock2.h>
+#include <ws2tcpip.h>
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+// Link with Ws2_32.lib
+#pragma comment(lib, "Ws2_32.lib")
+
+#define DEFAULT_PORT "4444"
+
+void init_windows_socket()
+{
+    WSADATA wsa_data;
+    // Techically after this line is run, wsa_data will store the version variable (2.2)
+    int i_result = WSAStartup(MAKEWORD(2, 2), &wsa_data);
+
+    // Remember in C++ 0 means no error, other values mean error.
+    if (i_result != 0)
+    {
+        std::cerr << "WSAStartup could be started properly !!\n";
+        exit(1);
+    }
+
+}
+
+SOCKET create_socket()
+{
+    struct addrinfo *result = nullptr;
+    struct addrinfo hints = {};
+
+    memset(&hints, 0, sizeof(hints));
+
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_INET; // ipV4
+    hints.ai_socktype = SOCK_STREAM; // TCP
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_flags = AI_PASSIVE; // mean Listener not Connector
+
+    int i_result = getaddrinfo(
+            nullptr,            // can be bind to all local ips
+            DEFAULT_PORT,       // bind over this port
+            &hints,             // while address resolving, use features of hint
+            &result             // start point of linked list which stands for as a container stores the results.
+            );
+
+    if (i_result != 0)
+    {
+        std::cerr << "getaddrinfo failed: " << i_result << std::endl;
+        exit(1);
+    }
+
+    SOCKET server_socket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+
+    if (server_socket == INVALID_SOCKET)
+    {
+        std::cerr << "Socket creation was failed !!\n";
+        exit(1);
+    }
+
+    i_result = bind(server_socket, result->ai_addr, (int)result->ai_addrlen);
+
+    if (i_result == SOCKET_ERROR)
+    {
+        std::cerr << "Binding was failed !! \n";
+        exit(1);
+    }
+
+    return server_socket;
+}
+
+int main()
+{
+    init_windows_socket();
+
+    SOCKET server_socket = create_socket();
+
+    std::cout << "Socket is created and bound to port:" << DEFAULT_PORT << std::endl;
+
     return 0;
 }
